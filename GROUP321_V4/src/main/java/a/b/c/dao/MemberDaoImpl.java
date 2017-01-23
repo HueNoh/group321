90cca090d872a1e7dc52559946a222ee268ff905
HueNoh <gaisharr@gmail.com>
2017-01-23 15:12:46 +0900

    hsnoh - mapper, dao

--- GROUP321_V4/src/main/java/a/b/c/dao/MemberDaoImpl.java
+++ GROUP321_V4/src/main/java/a/b/c/dao/MemberDaoImpl.java
@@ -20,4 +20,28 @@ public class MemberDaoImpl implements MemberDaoInterface {
 		return mapper.loginChk(map);
 	}
 
+	@Override
+	public int chkIdDup(Map map) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		return mapper.chkIdDup(map);
+	}
+
+	@Override
+	public int insertMember(Map map) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		return mapper.insertMember(map);
+	}
+
+	@Override
+	public int deleteMember(Map map) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		return mapper.deleteMember(map);
+	}
+
+	@Override
+	public int insertBoard(Map map) {
+		MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+		return mapper.insertBoard(map);
+	}
+
 }
--- GROUP321_V4/src/main/java/a/b/c/dao/MemberDaoInterface.java
+++ GROUP321_V4/src/main/java/a/b/c/dao/MemberDaoInterface.java
@@ -4,4 +4,8 @@ import java.util.Map;
 
 public interface MemberDaoInterface {
 	public int loginChk(Map map);
+	public int chkIdDup(Map map);
+	public int insertMember(Map map);
+	public int deleteMember(Map map);
+	public int insertBoard(Map map);
 }
